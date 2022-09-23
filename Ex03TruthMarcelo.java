25ff24674df1f1a8d7a3ef3c8282ffb0377c56f6
ShuKurenai1854 <93104027+ShuKurenai1854@users.noreply.github.com>
2022-09-23 08:56:06 +0800

    Add files via upload

---
+++ Ex03TruthMarcelo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex03truthmarcelo;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class Ex03TruthMarcelo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+        public class Song {
+            String name;
+            String genre;
+            
+            public Song() {
+                
+            }
+        }
+}
