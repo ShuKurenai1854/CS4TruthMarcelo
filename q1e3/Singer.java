2e5c66cdb6bbe22692f686646a3653d852b5179c
ShuKurenai1854 <93104027+ShuKurenai1854@users.noreply.github.com>
2022-10-07 08:56:05 +0800

    Add files via upload

---
+++ q1e3/Singer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex03truthmarcelo;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class Singer {
+    String name;
+    int noOfPerformances;
+    double earnings;
+    Song favoriteSong;
+    
+    void performForAudience(byte number) {
+        
+    }
+}
